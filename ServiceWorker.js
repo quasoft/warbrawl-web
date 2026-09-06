b8b503296a0d983d67ee2606a6999b7cf31a69f3
QuaSoft <info@quasoft.net>
2026-09-06 15:31:53 +0300

    Bump version to 0.4.1, since it is not compatible with old cached 0.3.1 version anymore

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,12 +1,12 @@
-const cacheName = "warbrawl-0.3.1-983ea853dd5a457290e5d2b53d73825c";
+const cacheName = "warbrawl-0.4.1-2ed887aa1bfd46e0b1de78315cc49614";
 const contentToCache = [
     "index.html",
     "manifest.webmanifest",
     "TemplateData/style.css",
-    "Build/Web.data.unityweb?v=0.3.1-983ea853dd5a457290e5d2b53d73825c",
-    "Build/Web.framework.js.unityweb?v=0.3.1-983ea853dd5a457290e5d2b53d73825c",
-    "Build/Web.loader.js?v=0.3.1-983ea853dd5a457290e5d2b53d73825c",
-    "Build/Web.wasm.unityweb?v=0.3.1-983ea853dd5a457290e5d2b53d73825c"
+    "Build/Web.data.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
+    "Build/Web.framework.js.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
+    "Build/Web.loader.js?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
+    "Build/Web.wasm.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614"
 ];
 
 self.addEventListener('install', function (e) {
