512011acd62b151487d5a1a9fd24a7386f657df8
QuaSoft <info@quasoft.net>
2026-09-11 20:57:51 +0300

    Add 6 bow abilities and new sound effects

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,12 +1,12 @@
-const cacheName = "warbrawl-0.4.1-2ed887aa1bfd46e0b1de78315cc49614";
+const cacheName = "warbrawl-0.5.1-3e4c511d174b4167b4dd2789f9331035";
 const contentToCache = [
     "index.html",
     "manifest.webmanifest",
     "TemplateData/style.css",
-    "Build/Web.data.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
-    "Build/Web.framework.js.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
-    "Build/Web.loader.js?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614",
-    "Build/Web.wasm.unityweb?v=0.4.1-2ed887aa1bfd46e0b1de78315cc49614"
+    "Build/Web.data.unityweb?v=0.5.1-3e4c511d174b4167b4dd2789f9331035",
+    "Build/Web.framework.js.unityweb?v=0.5.1-3e4c511d174b4167b4dd2789f9331035",
+    "Build/Web.loader.js?v=0.5.1-3e4c511d174b4167b4dd2789f9331035",
+    "Build/Web.wasm.unityweb?v=0.5.1-3e4c511d174b4167b4dd2789f9331035"
 ];
 
 self.addEventListener('install', function (e) {
